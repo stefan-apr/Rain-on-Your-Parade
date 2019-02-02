6b6a7935faf4583e28927901a2ff8b1c61c9c2c5
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-02 13:39:39 -0800

    new column, react, and search radius to html; skeleton weather API call

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -8,3 +8,36 @@ var config = {
   messagingSenderId: "852639680516"
 };
 firebase.initializeApp(config);
+
+
+// WEATHER API
+
+$(/*SEARCH RESULTS CLICKS*/).on("click", function(event) {
+  var lat = ; //
+  var long = ;//
+  var time = ; //for historical data
+  var URL = "https://api.darksky.net/forecast/e3bf810172b3fa7c6960cc8b6769743c/";
+  var queryURL;
+  var wxresults;
+
+  event.preventDefault();
+  if (/* Event date <= 7 days */) {
+      queryURL = URL + lat + "," + long;
+  }
+  else { /* Event date > 7 days away*/
+      queryURL = URL + lat + "," + long + "," + time;
+  }
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  })
+      .then(function(response) {
+          wxresults = response.data;
+          console.log(queryURL);
+          console.log(wxresults);
+
+      })
+
+  }
+)
