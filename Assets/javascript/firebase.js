066b3efded414a9d5a44d14ec7725e44d4f2bdcc
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-03 19:27:00 -0800

    weather api rough draft, css, html

---
+++ Assets/javascript/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var firebaseconfig = {
+    apiKey: "AIzaSyCdr3ogLmQPzltt7gQ7f2oX1gukqGmO7Tc",
+    authDomain: "rainingonyourparade.firebaseapp.com",
+    databaseURL: "https://rainingonyourparade.firebaseio.com",
+    projectId: "rainingonyourparade",
+    storageBucket: "rainingonyourparade.appspot.com",
+    messagingSenderId: "852639680516"
+};
+
+firebase.initializeApp(firebaseconfig);
---
+++ Assets/javascript/placesAPI.js
@@ -0,0 +1,65 @@
+apiKey = "AIzaSyAQtNCDpVA6bXIzMTuaWcTSYw2yIWRPHww"
+
+
+
+var selectedLocationID;
+var coordinates = {};
+
+$("#event-location").keyup(function(){
+    $("#showPredictions").empty();
+    var locationInput = $(this).val();
+    console.log($(this).val());
+    
+    var autoLocationUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" 
+                            + locationInput + "&types=(cities)&key=" + apiKey;
+    console.log(autoLocationUrl);   
+
+    $.ajax({
+        url : autoLocationUrl,
+        method : 'GET'
+    }).then(function(autoLocationResponse){
+        autoLocationResponse.predictions.forEach(function(locationPrediction){
+            var predictionLink = $("<button>", {
+                'data-placeId' : locationPrediction.place_id,
+                'text' : locationPrediction.description,
+                'class' : 'predictionButtons'
+            })
+            console.log(predictionLink);
+            $("#showPredictions").append(predictionLink);
+        })
+    })
+})
+
+$(document).on("click", ".predictionButtons", function(event){
+    event.preventDefault();
+    //console.log($(this).attr('data-placeId'));
+    selectedLocationID = $(this).attr('data-placeId');
+    coordinateUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?placeid=" 
+                    + selectedLocationID + "&key=" + apiKey;
+    //console.log(coordinateUrl);
+    $.ajax({
+        url : coordinateUrl,
+        method : 'GET'
+    }).then(function(selectedCoordinate){
+        //console.log(selectedCoordinate.result.geometry.location);
+        coordinates = selectedCoordinate.result.geometry.location;
+        //console.log(coordinates);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+// https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJRdmfADq_woARYaVhnfQSUTI&key=AIzaSyAQtNCDpVA6bXIzMTuaWcTSYw2yIWRPHww
+
+
+
+
+//"https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" + "" + "&types=(cities)&key=apiKey";
