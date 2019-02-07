7c5ac53276994a711ef189da1a33fe612e81ddb4
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-06 20:20:23 -0800

    weather icons to weather api, css, and skycons.js

--- Assets/javascript/weatherAPI.js
+++ Assets/javascript/weatherAPI.js
@@ -1,27 +1,25 @@
-// default is collapsed
+// weather results div default is collapsed
 
 $(document).on("click", ".result-shell", function(event) {
     event.preventDefault();
     
-    // if ($("this").next("div").hasClass("collapse")) {
+    if ($("this").next("div").hasClass("collapse")) {
     
         var lat = $(this).attr("data-latitude"); // pulled from eventbrite API
         var long = $(this).attr("data-longitude");// pulled from eventbrite API
-        var eventDate = moment($(this).attr("data-start"));
+        var eventDate = moment($(this).attr("data-start")).format("x"); // convert to UNIX format
 
         var today = moment();
         var URL = "https://api.darksky.net/forecast/e3bf810172b3fa7c6960cc8b6769743c/";
         var queryURL;
         var wxresults; 
         var wxdisplay = $("div");
-        var eventDate;
     
         if (eventDate.diff(today, "days") <= 7) {
             queryURL = URL + lat + "," + long;
         }
-        else { /* Event date > 7 days away*/
-            // I commented this line out because the "time" variable isn't declared and I don't know what it's supposed to be. -Stefan
-            // queryURL = URL + lat + "," + long + "," + time;
+        else {  // Event date > 7 days away, use historical data because forecast data not available
+            queryURL = URL + lat + "," + long + "," + eventDate;
         }
     
         $.ajax({
@@ -41,16 +39,14 @@ $(document).on("click", ".result-shell", function(event) {
                     var humid = wxresults.daily.data[i].humidity;
                     var wind = wxresults.daily.data[i].windSpeed;
                     var cloud = wxresults.daily.data[i].cloudCover;
-                    wxdisplay.append(summary + "<br>Rain: " + rain + "<br>High: " + high + "F<br>Low: " + low + "F<br>Humidity: " + humid + "<br>Wind speed: " + wind + "mph<br>Cloud cover: " + cloud + "<br>");
+                    wxdisplay.append(summary + "<br>Rain: " + rain + "<br>High: " + high + "\xB0F<br>Low: " + low + "\xB0F<br>Humidity: " + humid + "<br>Wind speed: " + wind + "mph<br>Cloud cover: " + cloud + "<br>");
                 }
-                $("this").append(wxdisplay);
-                $("this").attr("class", "collapse-show");
+                $("this").next("div").append(wxdisplay);
+                $("this").next("div").attr("class", "collapse-show");  // shows the div once it's populated with weather data
             })
 
-    /*
-    else {
-        // collapse div
-        $("this").attr("class", "collapse");
+    else {  // collapse div if it's already showing
+        $("this").next("div").attr("class", "collapse");
     }
-    */
+}
 })
