eebabf07e5167c89d5f4119925e243eb026ae84b
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-10 23:25:06 -0800

    changes to html

--- Assets/javascript/skycons.js
+++ Assets/javascript/skycons.js
@@ -706,3 +706,14 @@
 
   global.Skycons = Skycons;
 }(this));
+
+var icons = new Skycons(),
+list  = [
+    "clear-day", "clear-night", "partly-cloudy-day",
+    "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+    "fog"
+],
+i;
+for(i = list.length; i--; )
+    icons.set(list[i], list[i]);
+icons.play();
--- Assets/javascript/weatherAPI.js
+++ Assets/javascript/weatherAPI.js
@@ -3,16 +3,15 @@
 $("#results-page").on("click", ".result-shell", function(event) {
     event.preventDefault();
     console.log("click on event");
-    // var that = $(this); there's an issue somewhere with $(this)
+    var that = $(this); //there's an issue somewhere with $(this)
     
-    if ($(this).children().hasClass("collapse")) {   // the .children(".result-interior") is the div that actually contains weather data, starts out collapsed by default
+    if (that.children().hasClass("collapse")) {   // the .children() is the div that actually contains weather data, starts out collapsed by default
         console.log("show weather");
 
-        var lat = $(this).attr("data-latitude"); // pulled from eventbrite API
-        var long = $(this).attr("data-longitude");// pulled from eventbrite API
-        var eventDate = moment($(this).attr("data-start")).format("X"); // pulled from eventbrite API, convert to UNIX format
+        var lat = that.attr("data-latitude"); // pulled from eventbrite API
+        var long = that.attr("data-longitude");// pulled from eventbrite API
+        var eventDate = moment(that.attr("data-start")).format("X"); // pulled from eventbrite API, convert to UNIX format
         var wxresults; 
-        var wxdisplay = $("div");
         var proxy = "https://cors-anywhere.herokuapp.com/";
         var queryURL = "https://api.darksky.net/forecast/e3bf810172b3fa7c6960cc8b6769743c/" + lat + "," + long + "," + eventDate;
     
@@ -25,6 +24,7 @@ $("#results-page").on("click", ".result-shell", function(event) {
                 console.log(wxresults);
                 console.log(queryURL);
 
+                var wxdisplay = $("div");
                 var icon = wxresults.icon;
                 var summary = wxresults.summary;
                 var rain = wxresults.precipProbability;
@@ -33,18 +33,6 @@ $("#results-page").on("click", ".result-shell", function(event) {
                 var humid = wxresults.humidity;
                 var wind = wxresults.windSpeed;
                 var cloud = wxresults.cloudCover;
-            
-                // weather icons
-                var icons = new Skycons(),
-                list  = [
-                    "clear-day", "clear-night", "partly-cloudy-day",
-                    "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
-                    "fog"
-                ],
-                i;
-                for(i = list.length; i--; )
-                    icons.set(list[i], list[i]);
-                icons.play();
                 
                 wxdisplay.append("<canvas id='" + icon + "' height='64' width='64'></canvas> " + summary +
                     "<br>High temperature: " + high +
@@ -55,15 +43,15 @@ $("#results-page").on("click", ".result-shell", function(event) {
                     "mph<br>Cloud cover: " + cloud +
                     "<br>");
                 
-                    $(this).children().hasClass("interior-result").append(wxdisplay);
+                that.children().hasClass("result-interior").append(wxdisplay);
             })
-            $(this).children().removeClass("collapse").addClass("collapse-show");  // shows the div once it's populated with weather data
+            that.children().removeClass("collapse").addClass("collapse-show");  // shows the div once it's populated with weather data
             
         }
 
-    else if ($(this).children().hasClass("collapse-show")) {  // collapse div if it's already showing
-        $(this).children().removeClass("collapse-show").addClass("collapse");
-        $(this).children().empty(); //clears the div
+    else if (that.children().hasClass("collapse-show")) {  // collapse div if it's already showing
+        that.children().removeClass("collapse-show").addClass("collapse");
+        that.children().empty(); //clears the div
         console.log("hide weather");
     }
     else {
