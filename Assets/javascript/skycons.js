06d20341351b41feb1f1a0f2bf75c4a1e4892f12
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-02-10 22:16:04 -0800

    somewhat functional weather api, minor changes to skycons and html

--- Assets/javascript/skycons.js
+++ Assets/javascript/skycons.js
@@ -706,16 +706,3 @@
 
   global.Skycons = Skycons;
 }(this));
-
-
-
-var icons = new Skycons(),
-		list  = [
-			"clear-day", "clear-night", "partly-cloudy-day",
-			"partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
-			"fog"
-		],
-		i;
-for(i = list.length; i--; )
-	icons.set(list[i], list[i]);
-icons.play();
--- Assets/javascript/weatherAPI.js
+++ Assets/javascript/weatherAPI.js
@@ -3,11 +3,10 @@
 $("#results-page").on("click", ".result-shell", function(event) {
     event.preventDefault();
     console.log("click on event");
+    // var that = $(this);
     
-    if ($(this).children(".result-interior").hasClass("collapse")) {   // the .children(".result-interior") is the div that actually contains weather data, starts out collapsed by default
+    if ($(this).children().hasClass("collapse")) {   // the .children(".result-interior") is the div that actually contains weather data, starts out collapsed by default
         console.log("show weather");
-
-        $(this).children(".result-interior").empty(); //clears the div
 
         var lat = $(this).attr("data-latitude"); // pulled from eventbrite API
         var long = $(this).attr("data-longitude");// pulled from eventbrite API
@@ -33,7 +32,19 @@ $("#results-page").on("click", ".result-shell", function(event) {
                 var low = wxresults.temperatureLow;
                 var humid = wxresults.humidity;
                 var wind = wxresults.windSpeed;
-                var cloud = wxresults.cloudCover;                   
+                var cloud = wxresults.cloudCover;
+                
+                // weather icons
+                var icons = new Skycons(),
+                list  = [
+                    "clear-day", "clear-night", "partly-cloudy-day",
+                    "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+                    "fog"
+                ],
+                i;
+                for(i = list.length; i--; )
+                    icons.set(list[i], list[i]);
+                icons.play();
                 
                 wxdisplay.append("<canvas id='" + icon + "' height='64' width='64'></canvas> " + summary +
                     "<br>High temperature: " + high +
@@ -44,15 +55,15 @@ $("#results-page").on("click", ".result-shell", function(event) {
                     "mph<br>Cloud cover: " + cloud +
                     "<br>");
                 
-                $(this).children(".result-interior").append(wxdisplay);
+                    $(this).children().hasClass("interior-result").append(wxdisplay);
             })
-            $(this).children(".result-interior").removeClass("collapse").addClass("collapse-show");  // shows the div once it's populated with weather data
+            $(this).children().removeClass("collapse").addClass("collapse-show");  // shows the div once it's populated with weather data
             
         }
 
-    else if ($(this).children(".result-interior").hasClass("collapse-show")) {  // collapse div if it's already showing
-        $(this).children(".result-interior").removeClass("collapse-show").addClass("collapse");
-        $(this).children(".result-interior").empty(); //clears the div
+    else if ($(this).children().hasClass("collapse-show")) {  // collapse div if it's already showing
+        $(this).children().removeClass("collapse-show").addClass("collapse");
+        $(this).children().empty(); //clears the div
         console.log("hide weather");
     }
     else {
